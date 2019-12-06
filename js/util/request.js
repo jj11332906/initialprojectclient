7eade9797455bc5abeb8a64157e967b82ae56874
weikejun <11332906@qq.com>
2019-12-06 17:00:59 +0800

    wkj

--- js/util/request.js
+++ js/util/request.js
@@ -69,3 +69,80 @@ request.prototype.get = function (callback) {
         }
     });
 };
+
+
+request.prototype.post = function (callback) {
+
+    console.log(this.url);
+    console.log(this.params);
+    console.log(this.async);
+    var access_token =  this.params.accessToken;
+    var request_url = this.url;
+    if(request_url.indexOf('?')===-1){
+        request_url+="?access_token="+access_token;
+    }else{
+        request_url+="&access_token="+access_token;
+    }
+    console.log(request_url);
+    $.ajax({
+        //请求方式
+        type: "POST",
+        //请求的媒体类型
+        contentType: "application/x-www-form-urlencoded",
+        //请求地址
+        url: request_url,
+        // beforeSend: function(request) {
+        //     request.setRequestHeader("Authorization",  access_token);
+        // },
+        // 数据，json字符串
+        data: this.params,
+        async: this.async,
+        //请求成功
+        success: function (result) {
+            console.log(result);
+            callback(result);
+        },
+        //请求失败，包含具体的错误信息
+        error: function (e) {
+            console.log(e.status);
+            console.log(e.responseText);
+        }
+    });
+};
+
+
+request.prototype.put = function (callback) {
+
+    console.log(this.url);
+    console.log(this.params);
+    console.log(this.async);
+    var access_token =  this.params.accessToken;
+    var request_url = this.url;
+    if(request_url.indexOf('?')===-1){
+        request_url+="?access_token="+access_token;
+    }else{
+        request_url+="&access_token="+access_token;
+    }
+    console.log(request_url);
+    $.ajax({
+        //请求方式
+        type: "PUT",
+        //请求的媒体类型
+        contentType: "application/x-www-form-urlencoded",
+        //请求地址
+        url: request_url,
+        // 数据，json字符串
+        data: this.params,
+        async: this.async,
+        //请求成功
+        success: function (result) {
+            console.log(result);
+            callback(result);
+        },
+        //请求失败，包含具体的错误信息
+        error: function (e) {
+            console.log(e.status);
+            console.log(e.responseText);
+        }
+    });
+};
